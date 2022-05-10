58d70f50f24cd2cf5ce329b1e5f0df820fad509e
Gil0105 <dsslima@hotmail.com>
2022-05-10 20:24:17 -0300

    Criando configuracao inicial

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.send("Hello world");
+});
+// * localhost
+// * 127.0.0.1
+app.listen(port, () => {
+  console.log("Nosso servidor está rodando na porta " + port);
+});
